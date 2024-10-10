1fbf0945ce934e50a2d28fc6482a50cd4ad380f2
Sawyer <117670150+shplok@users.noreply.github.com>
2024-10-10 19:55:11 -0400

    Add Palindrome Number Solution

---
+++ palindromenumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String xString = String.valueOf(x);
+        int n = xString.length();
+
+        for (int i = 0; i < n/2; i++) {
+          if (xString.charAt(i) != xString.charAt(n - i - 1)) {
+          return false;
+          }
+
+        }
+        return true;
+    }
+}
