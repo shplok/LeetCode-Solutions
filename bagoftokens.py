1d85f3a0df582f3846699e8bdbb4042731e4bc62
Sawyer <117670150+shplok@users.noreply.github.com>
2024-10-10 21:33:45 -0400

    Add Bag of Tokens Solution

---
+++ bagoftokens.py
@@ -0,0 +1,22 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        
+        score = 0
+        max_score = 0
+        tokens.sort()
+        left, right = 0, len(tokens) - 1
+
+        while left <= right:
+            if power >= tokens[left]:
+                power -= tokens[left]
+                score += 1
+                left += 1
+                max_score = max(max_score, score)
+            elif score > 0:
+                power += tokens[right]
+                score -= 1
+                right -= 1
+            else:
+                break
+
+        return max_score
