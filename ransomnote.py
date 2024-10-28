dc9319bdb4fd8b331de0146b4784f869b3f61e6e
Sawyer <117670150+shplok@users.noreply.github.com>
2024-10-28 13:49:36 -0400

    Add Ransom Note Solution

--- ransomnote.py
+++ ransomnote.py
@@ -1,4 +1,27 @@
+# SOLUTION 1
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return True if ransomNote in magazine else False
         
+        for char in ransomNote:
+            if char not in magazine:
+                return False
+            magazine = magazine.replace(char, "", 1)
+        return True
+
+# SOLUTION 2
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        charMap = {}
+        for char in magazine:
+            if char in charMap:
+                charMap[char] += 1
+            else:
+                charMap[char] = 1
+                
+        for char in ransomNote:
+            if char not in charMap or charMap[char] == 0:
+                return False
+            charMap[char] -= 1
+        return True
