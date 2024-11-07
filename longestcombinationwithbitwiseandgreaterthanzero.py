06f531d378cfd4c95a027456f5e1a340d5d92f2e
Sawyer <117670150+shplok@users.noreply.github.com>
2024-11-07 11:54:45 -0500

    Add Longest Combination with Bitwise AND Greater than Zero Solution

---
+++ longestcombinationwithbitwiseandgreaterthanzero.py
@@ -0,0 +1,11 @@
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        maxCount = 0
+
+        for i in range(24):
+            count = 0
+            for num in candidates:
+                if (num &(1 << i)) != 0:
+                    count += 1
+            maxCount = max(maxCount, count)
+        return maxCount
