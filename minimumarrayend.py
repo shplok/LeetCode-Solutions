6fcd8051750ac7edc19b43b65ad2e9edb6c55b8b
Sawyer <117670150+shplok@users.noreply.github.com>
2024-11-09 10:56:00 -0500

    Add Minimum Array End Solution

--- minimumarrayend.py
+++ minimumarrayend.py
@@ -6,3 +6,21 @@ class Solution:
             res = (res + 1) | x
             n -= 1
         return res
+    
+    # time limit exceeded as above code runs in O(n)
+
+
+    # below is O(log n) solution
+
+    class Solution:
+        def minEnd(self, n: int, x: int) -> int:
+            res = x
+            n -= 1
+            mask = 1
+
+            while n > 0:
+                if (mask & x) == 0:
+                    res |= (n & 1) * mask
+                    n >>= 1
+                mask <<= 1
+            return res
