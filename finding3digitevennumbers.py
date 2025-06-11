cc698866eff4ecd8067f4bb45ce202b387bba4ee
Sawyer Bowerman <117670150+shplok@users.noreply.github.com>
2025-06-11 15:26:10 -0400

    Add Finding 3-Digit Even Numbers Solution

---
+++ finding3digitevennumbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        res = set()
+        
+        for i in range(len(digits)):
+            for j in range(len(digits)):
+                for k in range(len(digits)):
+                    if i != j and j != k and k != i:
+                        a, b, c = digits[i], digits[j], digits[k]
+                        if a != 0 and c % 2 == 0:
+                            num = a * 100 + b * 10 + c
+                            res.add(num)                        
+
+        return sorted(res)
