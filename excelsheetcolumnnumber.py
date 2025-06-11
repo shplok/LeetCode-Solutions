b225a1095fa48f09be186e6e6b068950bd9deb6b
Sawyer Bowerman <117670150+shplok@users.noreply.github.com>
2025-06-11 11:26:49 -0400

    Add Excel Sheet Column Number Solution

--- excelsheetcolumnnumber.py
+++ excelsheetcolumnnumber.py
@@ -1,8 +1,8 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        # A = 1 -> AB = 28 -> A*26 + B(2) = 28
-        # ZY = 26 * Z(26) + Y(25)
-        # Essentially, for the length of columnTitle, all except the last index should be mult by 26
+        # A = 1 -> AB = 28 -> A*26^1 + B(2) = 28
+        # ZY = 26 * Z(26^1) + Y(25)
+        # Essentially, for the length of columnTitle, all except the last index should be mult by 26 to the power of position from the left
         # Last column should be added to final product
 
         prod = 0
