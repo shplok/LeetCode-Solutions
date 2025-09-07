c6d48b3f5784b56b97ba31cc0656ba593a15aa1a
Sawyer Bowerman <117670150+shplok@users.noreply.github.com>
2025-09-07 15:45:53 -0400

    Add Search in Rotated Sorted Array Solution

---
+++ searchinrotatedsortedarray.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # return i of target if in nums else -1
+
+        lo = 0
+        hi = len(nums) - 1
+        mid = -1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            else:
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+                
+        return -1
+        
